088300965cc08f174991b18e367dc2a1d191b94a
Yu Bingjiao <yubingjiaocn@users.noreply.github.com>
2024-02-26 22:36:41 +0800

    Add directory of upscaling models

--- serve.sh
+++ serve.sh
@@ -9,13 +9,23 @@ if [ ! -z "$CONFIG_FILE" ]; then
 fi
 
 ACCELERATE=true exec python ./launch.py -f \
-    $SETTINGS --api $NOWEBUI --no-gradio-queue \
+    $SETTINGS --api --api-log $NOWEBUI --no-gradio-queue \
     --skip-prepare-environment --no-hashing \
     --no-download-sd-model \
+    --loglevel $LOGLEVEL \
     --listen --port $PORT --xformers \
     --ckpt /tmp/models/stable-diffusion/$SD_MODEL_CHECKPOINT \
     --ckpt-dir /tmp/models/stable-diffusion \
     --controlnet-dir /tmp/models/controlnet \
     --lora-dir /tmp/models/lora \
     --vae-dir /tmp/models/vae \
-    --embeddings-dir /tmp/models/embeddings $EXTRA_CMD_ARG
+    --embeddings-dir /tmp/models/embeddings \
+    --codeformer-models-path /tmp/models/Codeformer \
+    --gfpgan-models-path /tmp/models/GFPGAN \
+    --esrgan-models-path /tmp/models/ESRGAN \
+    --bsrgan-models-path /tmp/models/BSRGAN \
+    --realesrgan-models-path /tmp/models/RealESRGAN \
+    --scunet-models-path /tmp/models/ScuNET \
+    --swinir-models-path /tmp/models/SwinIR \
+    --ldsr-models-path /tmp/models/LDSR \
+    --clip-models-path /tmp/models/CLIP $EXTRA_CMD_ARG
