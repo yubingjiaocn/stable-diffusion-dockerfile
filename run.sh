3261a22e376d92dfd83b056ea72227c959b2c29b
Bingjiao Yu <yubingjiaocn@gmail.com>
2024-03-13 02:58:37 +0000

    Change mount location

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 docker run --gpus all -it \
--v /home/ubuntu/models:/tmp/models \
+-v /home/ubuntu/models:/opt/ml/code/models \
 -v /home/ubuntu/models/animatediff-model:/opt/ml/code/extensions/sd-webui-animatediff/model/ \
 -e SD_MODEL_CHECKPOINT="v1-5-pruned-emaonly.safetensors" \
 -p:8000:8080 sd-web-ui:v1.7.0-20240307-gif
