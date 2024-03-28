42adc75afc0c12bd5215f5213b79cd6a0715b9c9
Bingjiao Yu <yubingjiaocn@gmail.com>
2024-03-28 12:31:51 +0000

    Disable mmap for ComfyUI

--- comfyui/run.sh
+++ comfyui/run.sh
@@ -1,3 +1,3 @@
 docker run --gpus all -it \
--v /home/ubuntu/models:/opt/ml/code/models \
+-v /home/ubuntu/models-s3/:/opt/ml/code/models \
 -p:8000:8080 comfyui:20240313
--- sdwebui/run.sh
+++ sdwebui/run.sh
@@ -2,4 +2,4 @@ docker run --gpus all -it \
 -v /home/ubuntu/models:/opt/ml/code/models \
 -v /home/ubuntu/models/animatediff-model:/opt/ml/code/extensions/sd-webui-animatediff/model/ \
 -e SD_MODEL_CHECKPOINT="v1-5-pruned-emaonly.safetensors" \
--p:8000:8080 sd-web-ui:v1.7.0-20240307-gif
+-p:8000:8080 sd-web-ui:v1.8.0-20240312
